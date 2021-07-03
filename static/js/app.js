a6410e5b0ff6b498a32e049881bdb3b93e747b8a
rketchum95 <79775250+rketchum95@users.noreply.github.com>
2021-07-02 19:34:51 -0700

    Updated

--- static/js/app.js
+++ static/js/app.js
@@ -1,35 +1,53 @@
+
 function init() {
   d3.json("data/samples.json").then((data) => {
-    // console.log(samples)
-    var name = data.names;
+    samples = data.names;
+    samples.map(samples => +samples)
     var dropdownMenu = d3.select("#selDataset");
-    console.log(name);
+    console.log(samples);
 
-    name.forEach((sample) =>
+    samples.forEach((sample) =>
       dropdownMenu
       .append("idSelect")
-      .text(sample)
-      .property("value", sample));
+      .attr("key", sample)
+      .text(sample));
   });
-};
 
 let idSelect = "940";
 
 getData(idSelect);
 
+};
+
+// let idSelect = "940";
+
+// getData(idSelect);
+
 d3.selectAll("#selDataset").on("change", getData);
 
-function getData() {
+function getData(idSelect) {
   d3.json("data/samples.json").then((metaData) => {
-    var ethnicity = metaData.metadata.ethnicity;
-    // var gender
+    console.log(metaData);
+    let idSelect = "940";
+    var arr = metaData.metadata.filter(sampleObj => idSelect);
+    console.log(arr);
+    var result = arr[0];
+    var panelData = d3.select("#sample-metadata");
+
+    Object.entries(result).forEach(([key,value]) => {
+      panelData.append("div").text('${key}: ${value}');
+    });
+
+    // let gender = metaData.metadata.filter((val) => val.id == idSelect);
+    // var genderid = gender[3];
+    // console.log(genderid);
     // var age 
     // var location
     // var bbtype
     // var wfreq
-    console.log(ethnicity);
+    // console.log();
 
   });
-}
+};
 
 init();
